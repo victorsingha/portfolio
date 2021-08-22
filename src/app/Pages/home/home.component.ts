8bb771d6505845349ac28f1b8fe2ed8faf9073d4
victor-singha <vicunite2@gmail.com>
2021-08-22 16:33:55 +0530

    api call

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getData();
+  }
+
+  getData(){
+    this.http.get("https://raw.githubusercontent.com/victorsingha/Employee-Payroll-App/main/employee.json")
+    .subscribe(res => {
+      console.log(res)
+    })
   }
 
 }
