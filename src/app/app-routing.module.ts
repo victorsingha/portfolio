d8b49407ebe031f9244c93a8543e8ecd5ff6bffb
victor-singha <vicunite2@gmail.com>
2021-08-27 09:53:31 +0530

    projecs page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Pages/home/home.component';
+import { ProjectsComponent } from './Pages/projects/projects.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Pages/home/home.component';
+import { ProjectsComponent } from './Pages/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
